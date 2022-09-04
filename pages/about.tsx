d2acf031a8b0270164bb24caa8a4388006304248
bcthakre <bcthakregcp@gmail.com>
2022-09-04 12:56:10 -0500

    added text to index.js and created About page

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis dolor beatae dolore corrupti cumque, ex necessitatibus, adipisci molestias, ipsa similique aspernatur tempore vitae odio enim ab modi fugiat rerum odit!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime quisquam vel provident ut nulla veniam. Laboriosam nobis similique impedit cum voluptates explicabo provident id nisi, pariatur quod deserunt, doloribus dolorem.</p>
+        </div>
+    ) ;
+}
+
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ const Home: NextPage = () => {
   return (
     <div>
       <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus suscipit voluptas quidem exercitationem. Ratione nemo et fuga modi molestiae dolorem, minima quasi, ducimus unde asperiores aliquid cumque minus, error nihil?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet assumenda, tempora animi veniam aut ratione tenetur! Fugiat maxime, nesciunt minima, amet ratione incidunt cumque, sunt expedita quam consequuntur corporis et!</p>
     </div>
   )
 }
