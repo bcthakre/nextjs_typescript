b1a9960dca546e88d81e017c8ad6f5820be5347b
bcthakre <bcthakregcp@gmail.com>
2022-09-04 14:04:04 -0500

    added js file as well

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,12 +9,12 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div>
-      <Navbar />
+      
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus suscipit voluptas quidem exercitationem. Ratione nemo et fuga modi molestiae dolorem, minima quasi, ducimus unde asperiores aliquid cumque minus, error nihil?</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet assumenda, tempora animi veniam aut ratione tenetur! Fugiat maxime, nesciunt minima, amet ratione incidunt cumque, sunt expedita quam consequuntur corporis et!</p>
       <Link href="/ninjas"><a>See Ninja Listing</a></Link>
-      <Footer />
+      
     </div>
   )
 }
