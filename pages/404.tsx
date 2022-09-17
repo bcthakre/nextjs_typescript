344d22c21a780118604e9f418aad71e7ccd69d4a
bcthakre <bcthakregcp@gmail.com>
2022-09-17 15:18:53 -0500

    updated 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,19 @@
 import Link from "next/link";
-import styles from '../styles/Home.module.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+
+    const router = useRouter() ;
+
+    useEffect(() => {
+
+        setTimeout(() => {
+            router.push('/')
+        }, 3000)
+
+    },[router])
+
     return (
         <div className="not-found">
             <h1> Ooops</h1>
