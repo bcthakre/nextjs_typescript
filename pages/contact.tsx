e2e6ac83c09e1ff6c9d8e530785d531f286560bb
bcthakre <bcthakregcp@gmail.com>
2022-09-17 16:18:28 -0500

    Added new page

---
+++ pages/contact.tsx
@@ -0,0 +1,14 @@
+
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact Page</h1>
+            <h2> Email ID: test@gmail.com</h2>
+        </div>
+    )
+}
+
+export default Contact;
+
+
