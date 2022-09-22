57c8ae8dc851b3c9ebdf010acfeab4f9e360494d
bcthakre <bcthakregcp@gmail.com>
2022-09-22 11:51:24 -0500

    Added styling. Beware of naming module file. Dont say modules.css. instead module.css

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import styles from '../../styles/Ninjas.module.css';
 
 export const getStaticProps = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -20,7 +21,7 @@ const Ninjas = ({ ninjas }) => {
         <h1> All Ninjas</h1>
         {ninjas.map((ninja) => (
           <div key={ninja.id}>
-            <a>
+            <a className={styles.single}>
               <h3>{ninja.name}</h3>
             </a>
           </div>
